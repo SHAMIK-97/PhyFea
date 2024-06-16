b17748941fc38d836fc286100aef4904043e7ff9
Shabasu <shamik.basu@studio.unibo.it>
2024-06-17 01:33:31 +0200

    minor changes

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -4,31 +4,26 @@
 #SBATCH --error=/cluster/work/cvl/shbasu/phyfeaSegformer/results/phyFea_segformer_training_error.log
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=shamik.basu@studio.unibo.it
-#SBATCH --gpus=8
-#SBATCH --gres=gpumem:40g
-#SBATCH --mem-per-cpu=8G
-#sbatch --tmp=80G
-#SBATCH --ntasks=8
+#SBATCH --gpus=rtx_3090:4
+#SBATCH --mem-per-cpu=10G
+#SBATCH --ntasks=4
+#SBATCH --time=1:00:00
 
 CONFIG=$1
-GPUS=8
-PORT=15661
-
-
-
-CUDA_HOME=/cluster/apps/gcc-6.3.0/cuda-11.0.3-qdlibd2luz2fy7izfefao4c5yitxwjus
+GPUS=4
+PORT=$(shuf -i 15661-55661 -n 1)
 
 source /cluster/apps/local/env2lmod.sh
 module load gcc/6.3.0 python_gpu/3.8.5 eth_proxy
 
-/var/spool/slurm/slurmd/state/job62036655/
+
+
 
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname $0)/SegFormer/tools/train.py\
-   $(dirname $0)/SegFormer/local_configs/segformer/B4/phyFeaSegformer_B4_city_1024.py\
+    /cluster/work/cvl/shbasu/phyfeaSegformer/models/SegFormer/tools/train.py\
+   /cluster/work/cvl/shbasu/phyfeaSegformer/models/SegFormer/local_configs/segformer/B4/phyFeaSegformer_B4_city_1024.py\
  --work-dir=/cluster/work/cvl/shbasu/phyfeaSegformer/results\
  --launcher='pytorch' --load-from='/cluster/work/cvl/shbasu/phyfeaSegformer/models/pretrained_models/mit_b4.pth'\
- --resume-from='/cluster/work/cvl/shbasu/phyfeaSegformer/models/pretrained_models/mit_b4.pth'\
- --gpus=8 --seed=2022
+ --gpus=4 --seed=2022
